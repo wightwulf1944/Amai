63152e1db925b52549caccc087e966908f7ee4f7
Subaru "Shiro" Tashiro <subaru.tashiro@novare.com.hk>
2019-07-06 22:19:11 +0800

    WIP floating toolbar

---
+++ app/src/main/java/i/am/shiro/amai/widget/TopFloatingBehavior.java
@@ -0,0 +1,40 @@
+package i.am.shiro.amai.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.core.math.MathUtils;
+import androidx.core.view.ViewCompat;
+
+public final class TopFloatingBehavior extends CoordinatorLayout.Behavior<View> {
+
+    public TopFloatingBehavior(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onStartNestedScroll(@NonNull CoordinatorLayout coordinatorLayout,
+                                       @NonNull View child, @NonNull View directTargetChild,
+                                       @NonNull View target, int axes, int type) {
+        return axes == ViewCompat.SCROLL_AXIS_VERTICAL ||
+                super.onStartNestedScroll(coordinatorLayout, child, directTargetChild, target, axes, type);
+    }
+
+    @Override
+    public void onNestedScroll(@NonNull CoordinatorLayout coordinatorLayout, @NonNull View child,
+                               @NonNull View target, int dxConsumed, int dyConsumed,
+                               int dxUnconsumed, int dyUnconsumed, int type,
+                               @NonNull int[] consumed) {
+        super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed, type, consumed);
+
+        float translationY = MathUtils.clamp(
+                child.getTranslationY() - dyConsumed,
+                child.getBottom() * -1,
+                0);
+
+        child.setTranslationY(translationY);
+    }
+}
