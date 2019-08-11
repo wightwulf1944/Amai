4524e23658f2add519d9998ec6a41e3039f6e64f
Subaru "Shiro" Tashiro <subaru.tashiro@novare.com.hk>
2019-08-12 02:30:39 +0800

    implement DownloadsFragment ViewModel

--- app/src/main/java/i/am/shiro/amai/fragment/DownloadsFragment.java
+++ app/src/main/java/i/am/shiro/amai/fragment/DownloadsFragment.java
@@ -6,45 +6,28 @@ import android.view.View;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.RecyclerView;
 
 import i.am.shiro.amai.R;
 import i.am.shiro.amai.adapter.DownloadJobAdapter;
-import i.am.shiro.amai.model.DownloadJob;
-import io.realm.Realm;
-import io.realm.RealmResults;
+import i.am.shiro.amai.viewmodel.DownloadsFragmentModel;
 
 public class DownloadsFragment extends Fragment {
-
-    private Realm realm;
-
-    private RealmResults<DownloadJob> downloadJobs;
 
     public DownloadsFragment() {
         super(R.layout.fragment_downloads);
     }
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        realm = Realm.getDefaultInstance();
-        downloadJobs = realm.where(DownloadJob.class)
-                .findAll();
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        realm.close();
-    }
-
-    @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         DownloadJobAdapter adapter = new DownloadJobAdapter();
-        adapter.submitList(downloadJobs);
-        downloadJobs.addChangeListener(list -> adapter.submitList(list));
 
         RecyclerView recyclerView = view.findViewById(R.id.recyclerView);
         recyclerView.setAdapter(adapter);
+
+        ViewModelProviders.of(this)
+            .get(DownloadsFragmentModel.class)
+            .observeDownloadJobs(this, adapter::submitList);
     }
 }
