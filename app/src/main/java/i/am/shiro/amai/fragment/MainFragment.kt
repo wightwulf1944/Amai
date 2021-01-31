1adac023bc72053e512d7496ae300773ad0691a6
wightwulf1944 <wightwulf1944@gmail.com>
2021-02-01 00:36:27 +0800

    cleanup MainFragmnet

--- app/src/main/java/i/am/shiro/amai/fragment/MainFragment.kt
+++ app/src/main/java/i/am/shiro/amai/fragment/MainFragment.kt
@@ -2,94 +2,88 @@ package i.am.shiro.amai.fragment
 
 import android.os.Bundle
 import android.os.SystemClock
-import android.view.KeyEvent
 import android.view.View
-import androidx.annotation.IdRes
+import androidx.activity.addCallback
 import androidx.fragment.app.Fragment
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.fragment.app.commit
+import androidx.fragment.app.commitNow
 import com.google.android.material.snackbar.Snackbar
 import i.am.shiro.amai.R
 import kotlinx.android.synthetic.main.fragment_main.*
 
+private const val SAVED = "saved"
+private const val NHENTAI = "nhentai"
+private const val DOWNLOADS = "downloads"
+
 class MainFragment : Fragment(R.layout.fragment_main) {
 
-    private var lastBackPressTime: Long = 0
+    private var lastBackPressTime = 0L
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        view.requestFocus()
-        view.setOnBackKeyListener(::onBackPress)
+    private lateinit var savedFragment: SavedFragment
 
-        navigation.setOnNavigateListener(::onNavigate)
+    private lateinit var nhentaiFragment: NhentaiFragment
 
-        if (childFragmentManager.findFragmentById(R.id.fragmentContainer) == null) {
-            navigation.selectedItemId = R.id.navigation_nhentai
+    private lateinit var downloadsFragment: DownloadsFragment
+
+    private var isNew = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        if (savedInstanceState == null) {
+            savedFragment = SavedFragment()
+            nhentaiFragment = NhentaiFragment()
+            downloadsFragment = DownloadsFragment()
+
+            childFragmentManager.commitNow {
+                add(R.id.fragmentContainer, savedFragment, SAVED)
+                add(R.id.fragmentContainer, nhentaiFragment, NHENTAI)
+                add(R.id.fragmentContainer, downloadsFragment, DOWNLOADS)
+                detach(savedFragment)
+                detach(downloadsFragment)
+            }
+
+            isNew = true
+        } else {
+            savedFragment = childFragmentManager.findFragmentByTag(SAVED) as SavedFragment
+            nhentaiFragment = childFragmentManager.findFragmentByTag(NHENTAI) as NhentaiFragment
+            downloadsFragment = childFragmentManager.findFragmentByTag(DOWNLOADS) as DownloadsFragment
         }
     }
 
-    private fun onBackPress() {
-        val now = SystemClock.elapsedRealtime()
-        if (now > lastBackPressTime + 1000) {
-            lastBackPressTime = now
-            Snackbar.make(requireView(), R.string.confirm_exit, Snackbar.LENGTH_SHORT)
-                .setAnchorView(R.id.navigation)
-                .show()
-        } else {
-            requireActivity().finish()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner) {
+            val now = SystemClock.elapsedRealtime()
+            if (now > lastBackPressTime + 1000) {
+                lastBackPressTime = now
+                Snackbar.make(view, R.string.confirm_exit, Snackbar.LENGTH_SHORT)
+                    .setAnchorView(navigation)
+                    .show()
+            } else {
+                requireActivity().finish()
+            }
+        }
+
+        if (isNew) navigation.selectedItemId = R.id.navigation_nhentai
+        navigation.setOnNavigationItemSelectedListener {
+            onNavigate(it.itemId)
+            true
         }
     }
 
     private fun onNavigate(itemId: Int) {
-        val fragmentTransaction = childFragmentManager.beginTransaction()
-
-        // detach previous fragment if any
-        val detachingFragment = childFragmentManager.primaryNavigationFragment
-        if (detachingFragment != null) {
-            fragmentTransaction.detach(detachingFragment)
+        val targetFragment = when (itemId) {
+            R.id.navigation_saved -> savedFragment
+            R.id.navigation_nhentai -> nhentaiFragment
+            R.id.navigation_downloads -> downloadsFragment
+            else -> throw IllegalArgumentException()
         }
 
-        val fragmentTag = getFragmentTag(itemId)
-        var attachingFragment = childFragmentManager.findFragmentByTag(fragmentTag)
-        if (attachingFragment == null) { // add fragment if it has not been added
-            attachingFragment = getFragment(itemId)
-            fragmentTransaction.add(R.id.fragmentContainer, attachingFragment, fragmentTag)
-        } else { // attach fragment if it has already been added
-            fragmentTransaction.attach(attachingFragment)
-        }
-        fragmentTransaction.setPrimaryNavigationFragment(attachingFragment)
-        fragmentTransaction.setReorderingAllowed(true)
-        fragmentTransaction.commit()
-    }
-
-    private fun getFragmentTag(@IdRes itemId: Int): String = when (itemId) {
-        R.id.navigation_saved -> SavedFragment::class.java.name
-        R.id.navigation_nhentai -> NhentaiFragment::class.java.name
-        R.id.navigation_downloads -> DownloadsFragment::class.java.name
-        else -> throw IllegalArgumentException("No corresponding fragment for given itemId")
-    }
-
-    private fun getFragment(@IdRes itemId: Int): Fragment = when (itemId) {
-        R.id.navigation_saved -> SavedFragment()
-        R.id.navigation_nhentai -> NhentaiFragment()
-        R.id.navigation_downloads -> DownloadsFragment()
-        else -> throw IllegalArgumentException("No corresponding fragment for given itemId")
-    }
-}
-
-private fun BottomNavigationView.setOnNavigateListener(listener: (Int) -> Unit) =
-    setOnNavigationItemSelectedListener {
-        listener(it.itemId)
-        true
-    }
-
-private fun View.setOnBackKeyListener(listener: () -> Unit) {
-    setOnKeyListener { _, keyCode, event ->
-        if (keyCode == KeyEvent.KEYCODE_BACK &&
-            event.action == KeyEvent.ACTION_DOWN &&
-            event.repeatCount == 0) {
-            listener()
-            true
-        } else {
-            false
+        childFragmentManager.commit {
+            for (fragment in arrayOf(savedFragment, nhentaiFragment, downloadsFragment)) {
+                if (fragment == targetFragment) attach(fragment) else detach(fragment)
+            }
+            setReorderingAllowed(true)
         }
     }
 }
