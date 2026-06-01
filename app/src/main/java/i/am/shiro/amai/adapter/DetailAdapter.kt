f430f96ace2a5d747a62114a57c44a91b5c82c05
Subaru "Shiro" Tashiro <wightwulf1944@gmail.com>
2026-06-02 05:07:46 +0800

    integrate compose for Detail view thumbnails

--- app/src/main/java/i/am/shiro/amai/adapter/DetailAdapter.kt
+++ app/src/main/java/i/am/shiro/amai/adapter/DetailAdapter.kt
@@ -1,22 +1,16 @@
 package i.am.shiro.amai.adapter
 
-import android.view.View
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed
-import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.core.view.updateLayoutParams
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import coil.load
 import com.xwray.groupie.GroupieAdapter
 import com.xwray.groupie.GroupieViewHolder
 import com.xwray.groupie.Item
-import com.xwray.groupie.viewbinding.BindableItem
 import i.am.shiro.amai.R
 import i.am.shiro.amai.compose.AmaiTheme
-import i.am.shiro.amai.databinding.ItemPreviewImageBinding
 import i.am.shiro.amai.model.DetailModel
 import i.am.shiro.amai.model.Thumbnail
 
@@ -59,25 +53,24 @@ class DetailAdapter(
 
     private inner class ThumbnailItem(
         private val thumbnail: Thumbnail
-    ) : BindableItem<ItemPreviewImageBinding>() {
+    ) : Item<GroupieViewHolder>() {
 
-        override fun getLayout() = R.layout.item_preview_image
+        override fun getLayout() = R.layout.item_detail_header
 
         override fun getSpanSize(spanCount: Int, position: Int) = 1
 
-        override fun initializeViewBinding(p0: View) = ItemPreviewImageBinding.bind(p0)
-
-        override fun bind(binding: ItemPreviewImageBinding, position: Int) {
-            binding.root.setOnClickListener {
-                onThumbnailClick(position - 1)
-            }
-
-            binding.thumbnailImage.updateLayoutParams<ConstraintLayout.LayoutParams> {
-                dimensionRatio = "${thumbnail.width}:${thumbnail.height}"
-            }
-
-            binding.thumbnailImage.load(thumbnail.url) {
-                allowRgb565(true)
+        override fun bind(vh: GroupieViewHolder, position: Int) {
+            val composeView = vh.itemView as ComposeView
+            composeView.setViewCompositionStrategy(DisposeOnViewTreeLifecycleDestroyed)
+            composeView.setContent {
+                AmaiTheme {
+                    Surface(color = MaterialTheme.colorScheme.background) {
+                        ThumbnailContent(
+                            thumbnail = thumbnail,
+                            onClick = { onThumbnailClick(position - 1) }
+                        )
+                    }
+                }
             }
         }
     }
---
+++ app/src/main/java/i/am/shiro/amai/adapter/ThumbnailContent.kt
@@ -0,0 +1,44 @@
+package i.am.shiro.amai.adapter
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import i.am.shiro.amai.model.Thumbnail
+
+@Composable
+fun ThumbnailContent(
+    thumbnail: Thumbnail,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Box(
+        modifier = modifier
+            .padding(horizontal = 4.dp)
+            .padding(bottom = 8.dp)
+            .aspectRatio(thumbnail.width.toFloat() / thumbnail.height.toFloat())
+            .background(MaterialTheme.colorScheme.surfaceBright)
+            .clickable(onClick = onClick)
+    ) {
+        AsyncImage(
+            model = ImageRequest.Builder(LocalContext.current)
+                .data(thumbnail.url)
+                .allowRgb565(true)
+                .crossfade(true)
+                .build(),
+            contentDescription = null,
+            contentScale = ContentScale.Fit,
+            modifier = Modifier.fillMaxSize()
+        )
+    }
+}
