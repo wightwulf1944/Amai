ee8f7c786efe770c1a9c9a034fcb2dfbcce71e99
Subaru "Shiro" Tashiro <wightwulf1944@gmail.com>
2026-06-21 18:20:05 +0800

    improve top app bar inset handling

--- app/src/main/java/i/am/shiro/amai/compose/common/TopBarContainer.kt
+++ app/src/main/java/i/am/shiro/amai/compose/common/TopBarContainer.kt
@@ -4,13 +4,13 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.WindowInsetsSides
+import androidx.compose.foundation.layout.add
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.union
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
@@ -25,17 +25,16 @@ fun TopBarContainer(
     val background = Brush.verticalGradient(
         listOf(MaterialTheme.colorScheme.surface, Color.Transparent)
     )
-    val insets = WindowInsets.safeDrawing
-        .only(WindowInsetsSides.Top + WindowInsetsSides.Horizontal)
+    val insets = TopAppBarDefaults.windowInsets
         .asSymmetricHorizontal(0.5f)
+        .add(WindowInsets(left = 16.dp, right = 16.dp, top = 4.dp))
+        .union(WindowInsets(top = 6.dp))
     Row(
         modifier = Modifier
             .fillMaxWidth()
             .padding(bottom = 8.dp)
             .background(background)
-            .windowInsetsPadding(insets)
-            .padding(horizontal = 16.dp)
-            .padding(top = 4.dp),
+            .windowInsetsPadding(insets),
         content = content
     )
 }
